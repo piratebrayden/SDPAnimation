d70d92bc52714ac01314eee7510f083bd1ad5aa7
Brayden Hass <braydenl.hass@gmail.com>
2025-12-02 20:40:50 -0500

    Codes changes

--- menu_file.cpp
+++ menu_file.cpp
@@ -1,7 +1,10 @@
 #include "FEHLCD.h"
 #include "FEHUtility.h"
 #include "FEHRandom.h"
-
+#include "menu_file.h"
+void Returntomenu()
+{
+}
 void CreateMenuplay()
 {
     // Example function to create a menu on the FEHLCD
@@ -11,66 +14,14 @@ void CreateMenuplay()
     LCD.SetFontColor(BLACK);
     LCD.DrawPixel(60, 60); // Draw menu item
     // Add more menu items as needed
-}void riteplay()
+}
+void writeplay()
 {
     LCD.SetFontColor(WHITE);
     LCD.SetFontScale(1.0);
     LCD.WriteAt("Play Game", 100, 60);
 }
-//This function should handle the process of clicking play and starting up the game.
-/*Previous code    int x, y;
-    bool clicked = LCD.Touch(&x, &y, true);
-    if (clicked)
-    {
-        if (x >= 50 && x <= 250 && y >= 50 && y <= 80)*/
-void leftclickplay()
 
-{
-// New click code
-float x, y;
-    while (true)
-    {   
-        //Set the x and y value equal to the box size of the menu buttons
-        if(LCD.Touch(&x, &y, true))
-        {
-            // Check if the touch is within the "Play Game" button area
-            if (x >= 50 && x <= 250 && y >= 50 && y <= 80)
-        
-
-        {
-            // Play Game button clicked
-            LCD.Clear(BLACK);
-            Sleep(500); // Pause to show highlight
-            LCD.SetFontColor(WHITE);
-            LCD.SetFontScale(1.0);  
-            LCD.WriteAt("Starting ", 80, 120);
-            Sleep(550);
-            LCD.WriteAt("         Game", 80, 120);
-            Sleep(600);
-            LCD.WriteAt("              .", 80, 120);
-             Sleep(650);
-            LCD.WriteAt("              ..", 80, 120);
-             Sleep(700);
-            LCD.WriteAt("              ...", 80, 120);
-             Sleep(750);
-            LCD.Update();
-            Sleep(1000); // Pause before starting the game
-            // Here you can add code to start the game
-        }
-    }
-}
-}
-
-/*void CreateMenu()
-{
-    // Example function to create a menu on the FEHLCD
-    LCD.SetFontColor(WHITE);
-    LCD.DrawRectangle(50, 50, 200, 30); // Draw menu box
-    LCD.SetFontScale(1.5);
-    LCD.SetFontColor(BLACK);
-    LCD.DrawPixel(60, 60); // Draw menu item
-    // Add more menu items as needed
-}*/
 void CreateMenustats()
 {
     // Example function to create a menu on the FEHLCD
@@ -80,55 +31,16 @@ void CreateMenustats()
     LCD.SetFontColor(BLACK);
     LCD.DrawPixel(60, 60); // Draw menu item
     // Add more menu items as needed
-
 }
-//This is refusing to write to screen, it is assumed that is is being covered in some way.
-//Display the text stats to screen
-void ritestats()
+// This is refusing to write to screen, it is assumed that is is being covered in some way.
+// Display the text stats to screen
+void writestats()
 {
     Sleep(10);
     LCD.SetFontColor(WHITE);
     LCD.SetFontScale(1.0);
-    LCD.WriteAt("Stats", 100, 80);
+    LCD.WriteAt("Stats", 120, 85);
 }
-void leftclickstat()
-//The function is non-functional
-{
-// Click to access status
-float x, y;
-    while (true)
-    {   
-        //Set the x and y value equal to the box size of the menu buttons
-        if(LCD.Touch(&x, &y, true))
-        {
-            // Check if the touch is within the "Play Game" button area
-            if (x >= 60 && x <= 128 && y >= 80 && y <= 180)
-        
-
-        {
-            // Play Game button clicked
-            LCD.Clear(BLACK);
-            Sleep(500); // Pause to show highlight
-            LCD.SetFontColor(WHITE);
-            LCD.SetFontScale(1.0);  
-            LCD.WriteAt("Starting ", 80, 120);
-            Sleep(550);
-            LCD.WriteAt("         Game", 80, 120);
-            Sleep(600);
-            LCD.WriteAt("              .", 80, 120);
-             Sleep(650);
-            LCD.WriteAt("              ..", 80, 120);
-             Sleep(700);
-            LCD.WriteAt("              ...", 80, 120);
-             Sleep(750);
-            LCD.Update();
-            Sleep(1000); // Pause before starting the game
-            // Here you can add code to start the game
-        }
-    }
-}
-}
-
 
 void CreateMenuinfo()
 {
@@ -140,7 +52,14 @@ void CreateMenuinfo()
     LCD.DrawPixel(60, 60); // Draw menu item
     // Add more menu items as needed
 }
-void CreateMenucredits()
+void writeInstruct()
+{
+    Sleep(10);
+    LCD.SetFontColor(WHITE);
+    LCD.SetFontScale(1.0);
+    LCD.WriteAt("Instructions", 80, 115);
+}
+void createMenuCredits()
 {
     // Example function to create a menu on the FEHLCD
     LCD.SetFontColor(WHITE);
@@ -149,6 +68,13 @@ void CreateMenucredits()
     LCD.SetFontColor(BLACK);
     LCD.DrawPixel(60, 60); // Draw menu item
     // Add more menu items as needed
+}
+void writecredits()
+{
+    Sleep(10);
+    LCD.SetFontColor(WHITE);
+    LCD.SetFontScale(1.0);
+    LCD.WriteAt("Credits", 100, 145);
 }
 void decalline()
 {
@@ -170,25 +96,204 @@ void decalline4()
     LCD.SetFontColor(WHITE);
     LCD.DrawLine(0, 30, 319, 30); // Draw line across screen
 }
+
+// Touch function
+void testTouch()
+{
+    // New click code
+    float x, y;
+    while (true)
+    {
+        // Set the x and y value equal to the box size of the menu buttons
+        if (LCD.Touch(&x, &y, true))
+        {
+            // Check if the touch is within the "Play Game" button area
+            if (GoToPlay(x, y) || GoToStats(x, y) || GoToInstruct(x, y) || GoToCredits(x, y))
+            {
+                break;
+            }
+        }
+    }
+}
+// Function to handle "play" button click
+bool GoToStats(float x, float y)
+{
+    if (x >= 50 && x <= 200 && y >= 60 && y <= 80)
+    {
+        // Play Game button clicked
+        LCD.Clear(BLACK);
+        Sleep(500); // Pause to show highlight
+        LCD.SetFontColor(WHITE);
+        LCD.SetFontScale(1.0);
+        LCD.WriteAt("Starting ", 80, 120);
+        Sleep(550);
+        LCD.WriteAt("         Game", 80, 120);
+        Sleep(600);
+        LCD.WriteAt("              .", 80, 120);
+        Sleep(650);
+        LCD.WriteAt("              ..", 80, 120);
+        Sleep(700);
+        LCD.WriteAt("              ...", 80, 120);
+        Sleep(750);
+        LCD.Update();
+        Sleep(1000); // Pause before starting the game
+        // Here you can add code to start the game
+        return true;
+    }
+    return false;
+}
+// Function to handle "stats" button click
+bool GoToPlay(float x, float y)
+{
+    if (x >= 60 && x <= 200 && y >= 80 && y <= 100)
+    {
+        // Play Game button clicked
+        // Please note that the error values are place holders and will need to be remove and remade.
+        LCD.Clear(BLACK);
+        LCD.DrawHorizontalLine(30, 0, 319); // Draw line across screen
+        Sleep(500);                         // Pause to show highlight
+        LCD.SetFontColor(WHITE);
+        LCD.SetFontScale(1.0);
+        LCD.WriteAt("Stats: ", 10, 10);
+        Sleep(550);
+        LCD.WriteAt("Wood:", 10, 50);
+        Sleep(600);
+        LCD.WriteAt("Metal:", 10, 70);
+        LCD.SetFontColor(RED);
+        LCD.WriteAt("ERROR", 80, 50);
+        LCD.SetFontColor(WHITE);
+        Sleep(650);
+        LCD.WriteAt("Food:", 10, 90);
+        LCD.SetFontColor(RED);
+        LCD.WriteAt("ERROR", 80, 70);
+        LCD.SetFontColor(WHITE);
+        Sleep(700);
+        LCD.WriteAt("Total score...", 10, 200);
+        LCD.SetFontColor(RED);
+        LCD.WriteAt("ERROR", 80, 90);
+        Sleep(750);
+        LCD.SetFontColor(RED);
+        LCD.WriteAt("ERROR", 180, 200);
+        LCD.Update();
+        Sleep(1000); // Pause before starting the game
+        // Here you can add code to start the game
+        return true;
+    }
+    return false;
+}
+// Activates on clicking instructions
+bool GoToInstruct(float x, float y)
+{
+    if (x >= 60 && x <= 200 && y >= 100 && y <= 140)
+    {
+        // Play Game button clicked
+        // Please note that the error values are place holders and will need to be remove and remade.
+        LCD.Clear(BLACK);
+        LCD.DrawHorizontalLine(30, 0, 319); // Draw line across screen
+        Sleep(100);                         // Pause to show highlight
+        LCD.SetFontColor(WHITE);
+        LCD.SetFontScale(1.0);
+        LCD.WriteAt("Info: ", 10, 10);
+        LCD.SetFontScale(.5);
+        Sleep(1000);
+        LCD.WriteAt("Hello and welcome to your new", 10, 50);
+        Sleep(950);
+        LCD.WriteAt("simulated Village and hopefully town.", 10, 60);
+        Sleep(900);
+        LCD.WriteAt("In this village you will learn", 10, 70);
+        Sleep(850);
+        LCD.WriteAt("The value of resource management", 10, 80);
+        Sleep(800);
+        LCD.SetFontColor(WHITE);
+        LCD.WriteAt("Through the lens of a small village", 10, 90);
+        Sleep(750);
+        LCD.WriteAt("You will need to gather three different resources", 10, 100);
+        Sleep(700);
+        LCD.WriteAt("-Wood  -Metal  -Food", 10, 110);
+        Sleep(650);
+        LCD.WriteAt("Each will have its own purpose and limitation", 10, 120);
+        Sleep(600);
+        LCD.WriteAt("And you will need to carefully balance them.", 10, 130);
+        Sleep(550);
+        LCD.WriteAt("Do to how you will be forced to spend time", 10, 140);
+        Sleep(500);
+        LCD.WriteAt("collecting each, and during these times", 10, 150);
+        Sleep(450);
+        LCD.WriteAt("other resources may deplete or be lost.", 10, 160);
+        Sleep(400);
+        LCD.WriteAt("So always be careful of what you have", 10, 170);
+        Sleep(350);
+        LCD.WriteAt("or else.", 10, 180);
+        Sleep(1000);
+        LCD.SetFontColor(RED);
+        LCD.WriteAt("Good Luck", 10, 200);
+        LCD.Update();
+        Sleep(1000);
+        // Pause before starting the game
+        // Here you can add code to start the game
+        return true;
+    }
+    return false;
+}
+// Function to handle "Credits" button click
+bool GoToCredits(float x, float y)
+{
+    if (x >= 50 && x <= 200 && y >= 140 && y <= 180)
+    {
+        // Play Game button clicked
+        LCD.Clear(BLACK);
+        LCD.DrawHorizontalLine(30, 0, 319); // Draw line across screen
+        Sleep(100);                         // Pause to show highlight
+        LCD.SetFontColor(WHITE);
+        LCD.SetFontScale(1.0);
+        LCD.WriteAt("Credits: ", 10, 10);
+        LCD.SetFontScale(0.5);
+        Sleep(750);
+        LCD.WriteAt("Code by: ", 10, 50);
+        Sleep(750);
+        LCD.WriteAt("Brayden Hass, and Glen Bush", 10, 70);
+        Sleep(750);
+        LCD.WriteAt("Made within:", 10, 90);
+        Sleep(750);
+        LCD.WriteAt("Visual Studio", 10, 110);
+        Sleep(750);
+        LCD.WriteAt("References:", 10, 130);
+        Sleep(750);
+        LCD.WriteAt("Paul Clingen, Taylor Hamilton, Rahul Sanghvi", 10, 150);
+        Sleep(750);
+        LCD.WriteAt("And a special thanks to ", 10, 170);
+        LCD.SetFontColor(RED);
+        LCD.WriteAt("                       you", 10, 170);
+        LCD.Update();
+        Sleep(1000); // Pause before starting the game
+        // Here you can add code to start the game
+        return true;
+    }
+    return false;
+}
+
 int main()
 {
-    // Clear background
-    LCD.SetBackgroundColor(BLACK);
-    LCD.Clear();
+    while (true)
+    {
+        // Clear background
+        LCD.SetBackgroundColor(BLACK);
+        LCD.Clear();
 
-    CreateMenuplay();
-    CreateMenuinfo();
-    CreateMenucredits();
-    CreateMenustats();
-    decalline();
-    decalline2();
-    decalline3();
-    decalline4();
-    riteplay();
-    leftclickplay();
-    leftclickstat();
-    ritestats();
-
+        CreateMenuplay();
+        CreateMenuinfo();
+        createMenuCredits();
+        CreateMenustats();
+        decalline();
+        decalline2();
+        decalline3();
+        decalline4();
+        writeplay();
+        writeInstruct();
+        writestats();
+        writecredits();
+        testTouch();
+    }
     
     while (1)
     {
---
+++ menu_file.h
@@ -0,0 +1,8 @@
+#pragma once
+
+bool GoToPlay(float x, float y);
+
+bool GoToStats(float x, float y);
+
+bool GoToInstruct(float x, float y);
+bool GoToCredits(float x, float y);
